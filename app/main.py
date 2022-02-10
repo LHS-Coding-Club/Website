ea23f6094dbd626c7ddb3b31dac3e6bce704f28c
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2022-02-10 05:40:14 -0600

    Added static file support.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 
 from app.routes.auth import auth_router
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/frontend", StaticFiles(directory="./app/frontend"), name="frontend")
+
+@app.get("/")
+async def home():
+    return FileResponse("./app/frontend/html/home.html")
+
 app.include_router(auth_router, prefix="/auth")

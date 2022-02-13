fd6d48a25eb1bfd9d6306ebca40f78157e1631b7
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2022-02-13 10:39:20 -0600

    Created background task to log http request date & time.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+import time
 
+from fastapi import FastAPI, BackgroundTasks 
 #from app.routes.auth import auth_router
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,8 +9,15 @@ app = FastAPI()
 
 app.mount("/frontend", StaticFiles(directory="./app/frontend"), name="frontend")
 
+def write_log():
+    request_time = time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime(time.time()))
+    log_file = open("logs.txt", "a")
+    log_file.write(f"Request made at: {request_time}\n")
+    log_file.close()
+
 @app.get("/")
-async def home():
+async def home(background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_log)
     return FileResponse("./app/frontend/html/home.html")
 
 #app.include_router(auth_router, prefix="/auth")
